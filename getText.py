3579dc6a3c06614e72e7c0c0c62008ed93e7fec9
naboagye <naboagye13@gmail.com>
2022-02-23 06:34:51 +0000

    first commit

---
+++ getText.py
@@ -0,0 +1,17 @@
+import requests
+
+page_id = "9f8b41acdfb34d31bb05000e8127a723"
+
+url = f"https://api.notion.com/v1/blocks/{page_id}/children?page_size=100"
+
+headers = {
+  'Notion-Version': '2021-05-13',
+  'Authorization': 'Bearer secret_vuOFavaPvgbS8iHRJa6vqtgosrugUVfpdiWsp8NtqkO'
+}
+
+response = requests.get(url, headers=headers)
+
+for result in response.json()["results"]:
+    if "paragraph" in result:
+        for text in result["paragraph"]["text"]:
+            print(text["plain_text"])
